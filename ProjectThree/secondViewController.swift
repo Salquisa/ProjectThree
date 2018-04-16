dbefd18f33915819603aa615f0d9584dfcce8bc6
CM Student <cmstudent@UHWO-ACM-C-07.local>
2018-04-16 10:54:27 -1000

    Reset the initial location. Added locations for the six restaurants

--- ProjectThree/secondViewController.swift
+++ ProjectThree/secondViewController.swift
@@ -16,8 +16,8 @@ class secondViewController: UIViewController {
     
     @IBOutlet var mapView: MKMapView!
     
-    let initialLocation = CLLocation(latitude: 21.391043, longitude: -158.017300)
-    let regionRadius: CLLocationDistance = 17000
+    let initialLocation = CLLocation(latitude: 21.383430, longitude: -158.005339)
+    let regionRadius: CLLocationDistance = 16000
     
     override func viewDidLoad() {
         
@@ -32,7 +32,21 @@ class secondViewController: UIViewController {
         
         let restaurantOne = Restaurant(title: "California Pizza Kitchen", type: "California Pizza Kitchen", coordinate: CLLocationCoordinate2D(latitude: 21.333612, longitude: -158.051134))
         mapView.addAnnotation(restaurantOne)
+        
+        let restaurantTwo = Restaurant(title: "Pieology", type: "Pieology", coordinate: CLLocationCoordinate2D(latitude: 21.385116, longitude: -157.941679))
+        mapView.addAnnotation(restaurantTwo)
+        
+        let restaurantThree = Restaurant(title: "Panda Express", type: "Panda Express", coordinate: CLLocationCoordinate2D(latitude: 21.426544, longitude: -158.001477))
+        mapView.addAnnotation(restaurantThree)
 
+        let restaurantFour = Restaurant(title: "Applebee's", type: "Applebee's", coordinate: CLLocationCoordinate2D(latitude: 21.333471, longitude: -158.052807))
+        mapView.addAnnotation(restaurantFour)
+        
+        let restaurantFive = Restaurant(title: "Johnny Rockets", type: "Johnny Rockets", coordinate: CLLocationCoordinate2D(latitude: 21.396613, longitude: -157.977040))
+        mapView.addAnnotation(restaurantFive)
+        
+        let restaurantSix = Restaurant(title: "Sushi Bay", type: "Sushi Bay", coordinate: CLLocationCoordinate2D(latitude: 21.339841, longitude: -158.078220))
+        mapView.addAnnotation(restaurantSix)
         
         // Do any additional setup after loading the view
         self.menuTitleTwo.title = "Location of Restaurants"
